afe2f1d6d48ffcfa72c2cc811f6e4bc64ae78380
lazyshark2001 <lazyshark2001@gmail.com>
2024-04-25 19:59:32 +0800

    new project

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Requirements
+
+pip install -r requirements.txt
+
+
+# Test
+python val.py
--- val.py
+++ val.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 if __name__ == '__main__':
     # Load a model
-    model = YOLO('weight/SCFNet.pt')
+    model = YOLO('./weight/SCFNet.pt')
 
     # Validate the model
     metrics = model.val(data='data.yaml')
